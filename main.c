730a3efb9dbd71c4e69737f0932b45ee1b1342f7
MehontsevKM <86251520+MehontsevKM@users.noreply.github.com>
2021-06-22 01:32:27 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+# включить  " bmp.h "
+# включить  < stdio.h >
+
+int  main ( int argc, char * argv []) {
+	если (argc> 1 )
+		читать (argv [ 1 ], argv [ 2 ]);
+	еще
+		printf ( " Не аргументы " );
+	возврат  0 ;
+}
