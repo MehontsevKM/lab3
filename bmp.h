76a7c3c3731b608dfe1e9a8b9d03b44245ae5aca
MehontsevKM <86251520+MehontsevKM@users.noreply.github.com>
2021-06-22 01:31:46 +0300

    Create bmp.h

---
+++ bmp.h
@@ -0,0 +1,8 @@
+# define  _CRT_SECURE_NO_WARNINGS
+# pragma once
+# включить  < stdio.h >
+# включить  < windows.h >
+# определить  WIN32_MEAN_AND_LEAN
+
+недействительное  чтение ( const  char * file_in, const  char * file_out);
+void  out (BITMAPFILEHEADER file_header, BITMAPINFOHEADER info_header, ФАЙЛ * вход, ФАЙЛ * выход);
